2e3e0d315a3405cdc21e597692243a124d57017e
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-09-21 20:23:13 -0600

    Chore: add resource files and organize windows
    Add resource files (.resx) for the internationalisation
    of text in the user interface.
    
    Existing windows are reorganised into specific folders
    (PopUps, GameWindows) to improve the structure and maintainability
    of the project.

--- UnoLisClient.UI/PopUpWindows/SimplePopUpWindow.xaml.cs
+++ UnoLisClient.UI/PopUpWindows/SimplePopUpWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UnoLisClient.UI
+namespace UnoLisClient.UI.PopUpWindows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SimplePopUpWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SimplePopUpWindow : Window
     {
-        public MainWindow()
+        public SimplePopUpWindow()
         {
             InitializeComponent();
         }
