dc46a4156e7f546d794c885a1e1854c617040ba6
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-09-24 22:00:25 -0600

    Refactor: improve the layout structure of pop-ups
    The structure uses a StackPanel to organize controls on pop-up windows.

--- UnoLisClient.UI/App.xaml.cs
+++ UnoLisClient.UI/App.xaml.cs
@@ -22,7 +22,7 @@ namespace UnoLisClient.UI
             string title = UnoLisClient.UI.Properties.Langs.Global.ConfirmationLabel;
             string message = UnoLisClient.UI.Properties.Langs.Global.ConfirmationMessageLabel;
             string watermark = UnoLisClient.UI.Properties.Langs.Global.CodeLabel;
-            var win = new InputPopUpWindow(title, message, watermark);
+            var win = new SimplePopUpWindow(title, message);
             win.Title = UnoLisClient.UI.Properties.Langs.Global.ConfirmationLabel;
             win.Show();
         }
