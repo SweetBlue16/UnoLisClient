58fb70c4e1c8f7c7562732676186e27020b81ec8
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-09-25 19:39:22 -0600

    Style: add combo box style
    Add style to ComboBoxes with behavior and animation depending on whether it's deselected, focused, etc.

--- UnoLisClient.UI/App.xaml.cs
+++ UnoLisClient.UI/App.xaml.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using UnoLisClient.UI.PopUpWindows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using UnoLisClient.UI.UnoLisWindows;
 
 namespace UnoLisClient.UI
 {
@@ -17,14 +19,6 @@ namespace UnoLisClient.UI
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-            // UI Tests
-            string title = UnoLisClient.UI.Properties.Langs.Global.WarningLabel;
-            string message = string.Format(UnoLisClient.UI.Properties.Langs.FriendsList.RemoveFriendMessageLabel, "SweetBlue16"); 
-            string watermark = UnoLisClient.UI.Properties.Langs.Global.CodeLabel;
-            var win = new QuestionPopUpWindow(title, message);
-            win.Title = UnoLisClient.UI.Properties.Langs.Global.WarningLabel.ToUpper();
-            win.Show();
         }
     }
 }
