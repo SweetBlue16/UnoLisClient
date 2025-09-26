a94e656fa08ca43ddc7e0e56b0dc811eab620a9f
Erick <erickmelvzlpz@gmail.com>
2025-09-26 05:39:42 -0600

    Added MainWindow, Frame for Navigation & Assets

---
+++ UnoLisClient.UI/UnoLisWindows/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace UnoLisClient.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void VideoBackground_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            VideoBackground.Position = TimeSpan.Zero;
+            VideoBackground.Play();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MusicPlayer.Play();
+            MainFrame.Navigate(new Pages.GamePage());
+        }
+        private void MusicPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MusicPlayer.Position = TimeSpan.Zero; // Reinicia la canción
+            MusicPlayer.Play();
+        }
+
+    }
+
+}
