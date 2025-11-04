0dbf7401e48d2b8ce365239f963cbea03ce468f5
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-11-04 15:47:59 -0600

    Refactored profile view functionality
    Introduces ProfileViewViewModel and supporting services/callbacks for loading and displaying user profile data. Refactors YourProfilePage to use MVVM, data binding, and command-based interactions. Adds generic RelayCommand<T>, improves BrowserHelper error handling, and updates project files to include new classes.

---
+++ UnoLisClient.Logic/Callbacks/ProfileViewCallback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel;
+using System.Windows;
+using UnoLisClient.Logic.UnoLisServerReference.ProfileView;
+using UnoLisServer.Common.Models;
+
+namespace UnoLisClient.Logic.Callbacks
+{
+    [CallbackBehavior(UseSynchronizationContext = false)]
+    public class ProfileViewCallback : IProfileViewManagerCallback
+    {
+        private readonly Action<ServiceResponse<ProfileData>> _onResponse;
+
+        public ProfileViewCallback(Action<ServiceResponse<ProfileData>> onResponse)
+        {
+            _onResponse = onResponse;
+        }
+
+        public void ProfileDataReceived(ServiceResponse<ProfileData> response)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                _onResponse?.Invoke(response);
+            });
+        }
+    }
+}
---
+++ UnoLisClient.Logic/Services/ProfileViewService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using UnoLisClient.Logic.Callbacks;
+using UnoLisClient.Logic.Helpers;
+using UnoLisClient.Logic.UnoLisServerReference.ProfileView;
+using UnoLisServer.Common.Models;
+
+namespace UnoLisClient.Logic.Services
+{
+    public class ProfileViewService
+    {
+        public Task<ServiceResponse<ProfileData>> GetProfileDataAsync(string nickname)
+        {
+            var taskCompletion = new TaskCompletionSource<ServiceResponse<ProfileData>>();
+            ProfileViewManagerClient profileViewClient = null;
+
+            try
+            {
+                Action<ServiceResponse<ProfileData>> callbackAction = (response) =>
+                {
+                    try
+                    {
+                        taskCompletion.TrySetResult(response);
+                    }
+                    finally
+                    {
+                        CloseClientHelper.CloseClient(profileViewClient);
+                    }
+                };
+                var callbackHandler = new ProfileViewCallback(callbackAction);
+                var context = new InstanceContext(callbackHandler);
+                profileViewClient = new ProfileViewManagerClient(context);
+
+                profileViewClient.GetProfileData(nickname);
+            }
+            catch (EndpointNotFoundException enfEx)
+            {
+                LogManager.Error("Error de conexión (ProfileView): No se encontró el endpoint.", enfEx);
+                taskCompletion.TrySetException(enfEx);
+                CloseClientHelper.CloseClient(profileViewClient);
+            }
+            catch (TimeoutException timeoutEx)
+            {
+                LogManager.Error("Error de conexión (ProfileView): Tiempo de espera agotado.", timeoutEx);
+                taskCompletion.TrySetException(timeoutEx);
+                CloseClientHelper.CloseClient(profileViewClient);
+            }
+            catch (CommunicationException commEx)
+            {
+                LogManager.Error("Error de comunicación al obtener los datos del perfil.", commEx);
+                taskCompletion.TrySetException(commEx);
+                CloseClientHelper.CloseClient(profileViewClient);
+            }
+            catch (Exception ex)
+            {
+                LogManager.Error("Error inesperado al obtener los datos del perfil.", ex);
+                taskCompletion.TrySetException(ex);
+                CloseClientHelper.CloseClient(profileViewClient);
+            }
+            return taskCompletion.Task;
+        }
+    }
+}
