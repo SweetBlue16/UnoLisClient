0fb8e1c7e3b895e65a7fa2f5f2263a043384a92f
Erick <erickmelvzlpz@gmail.com>
2025-12-08 07:32:38 -0600

    Added interface and Class for audio

---
+++ UnoLisClient.UI/Services/IAudioService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoLisClient.UI.Services
+{
+    public interface IAudioService
+    {
+        void SetMusicVolume(double volume);
+    }
+}
