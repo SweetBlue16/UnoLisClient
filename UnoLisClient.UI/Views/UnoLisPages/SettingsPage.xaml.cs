cdfdc4b7197e2b8f17a084535d5e507304796575
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-12-11 00:49:36 -0600

    Refactored Settings page to use MVVM pattern
    Moved logic from SettingsPage.xaml.cs to a new SettingsViewModel, implementing language and volume settings with data binding. Updated XAML to bind UI elements to the ViewModel and replaced code-behind event handlers with commands and property bindings. This improves maintainability and testability by adhering to the MVVM architecture.

--- UnoLisClient.UI/Views/UnoLisPages/SettingsPage.xaml.cs
+++ UnoLisClient.UI/Views/UnoLisPages/SettingsPage.xaml.cs
@@ -1,91 +1,34 @@
-﻿using System.Globalization;
-using System.Threading;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Threading;
-using UnoLisClient.UI.Views.UnoLisWindows;
+using UnoLisClient.UI.Services;
+using UnoLisClient.UI.Utilities;
+using UnoLisClient.UI.ViewModels;
 
 namespace UnoLisClient.UI.Views.UnoLisPages
 {
     /// <summary>
     /// Interaction logic for SettingsPage.xaml
     /// </summary>
-    public partial class SettingsPage : Page
+    public partial class SettingsPage : Page, INavigationService
     {
-        private bool _initializing = true;
-
         public SettingsPage()
         {
             InitializeComponent();
-
-            var saved = Properties.Settings.Default.languageCode;
-            if (string.IsNullOrWhiteSpace(saved))
-                saved = "en-US";
-
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo(saved);
-            Thread.CurrentThread.CurrentCulture = new CultureInfo(saved);
-            SelectLanguageByCode(saved);
-
-            if (Properties.Settings.Default.lastVolume > 0)
-                VolumeSlider.Value = Properties.Settings.Default.lastVolume;
-            else
-                VolumeSlider.Value = 50;
-
-            _initializing = false;
+            DataContext = new SettingsViewModel(this, new AlertManager());
         }
 
-        private void ClickCloseButton(object sender, RoutedEventArgs e)
+        public void NavigateTo(Page page)
         {
-            NavigationService?.Navigate(new MainMenuPage());
+            Application.Current.Dispatcher.Invoke(() =>
+                NavigationService?.Navigate(page)
+            );
         }
 
-        private void SelectLanguageByCode(string code)
+        public void GoBack()
         {
-            for (int i = 0; i < LanguageComboBox.Items.Count; i++)
-            {
-                if (LanguageComboBox.Items[i] is ComboBoxItem item &&
-                    item.Tag is string tag &&
-                    tag == code)
-                {
-                    LanguageComboBox.SelectedIndex = i;
-                    return;
-                }
-            }
-            LanguageComboBox.SelectedIndex = 0;
+            Application.Current.Dispatcher.Invoke(() =>
+                NavigationService?.GoBack()
+            );
         }
-
-        private void SelectionChangedLanguageComboBox(object sender, SelectionChangedEventArgs e)
-        {
-            if (_initializing) return;
-
-            if (LanguageComboBox.SelectedItem is ComboBoxItem item &&
-                item.Tag is string newLangCode)
-            {
-                if (Properties.Settings.Default.languageCode == newLangCode) return;
-
-                Properties.Settings.Default.languageCode = newLangCode;
-                Properties.Settings.Default.Save();
-
-                Thread.CurrentThread.CurrentUICulture = new CultureInfo(newLangCode);
-                Thread.CurrentThread.CurrentCulture = new CultureInfo(newLangCode);
-
-                Dispatcher.BeginInvoke(new System.Action(() =>
-                {
-                    NavigationService?.Navigate(new SettingsPage());
-                }), DispatcherPriority.ApplicationIdle);
-            }
-        }
-
-        private void ValueChangedVolumeSlider(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (_initializing) return;
-
-            var mainWindow = Application.Current.MainWindow as MainWindow;
-            mainWindow?.SetMusicVolume(e.NewValue);
-
-            Properties.Settings.Default.lastVolume = e.NewValue;
-            Properties.Settings.Default.Save();
-        }
-
     }
 }
