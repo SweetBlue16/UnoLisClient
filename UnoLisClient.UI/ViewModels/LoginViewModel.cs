fa13ea89a650895f9ccaa546bd8ea4f6140a78e2
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-12-11 01:32:17 -0600

    Removed unused using directives in LoginViewModel
    Cleaned up LoginViewModel.cs by removing unnecessary using statements to improve code clarity and maintainability.

--- UnoLisClient.UI/ViewModels/LoginViewModel.cs
+++ UnoLisClient.UI/ViewModels/LoginViewModel.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Linq;
 using System.ServiceModel;
-using System.ServiceModel.Channels;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Threading;
 using UnoLisClient.Logic.Enums;
-using UnoLisClient.Logic.Helpers;
 using UnoLisClient.Logic.Models;
 using UnoLisClient.Logic.Services;
 using UnoLisClient.Logic.UnoLisServerReference.Login;
